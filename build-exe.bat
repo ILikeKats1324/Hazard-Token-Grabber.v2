090169b94a50026d131d41d52a3a2d93b6e1fe42
ILikeKats1324 <111326553+ILikeKats1324@users.noreply.github.com>
2022-08-15 22:39:48 -0400

    Update build-exe.bat

--- build-exe.bat
+++ build-exe.bat
@@ -1,26 +1,26 @@
 @echo off
 color 0a
 echo.
-set /p a="Enter the exe name : "
-if [%a%]==[] ( 
+set /p a="Enter the exe name : 10015
+if [10015]==[] ( 
     CALL:error
     pause
     EXIT /B
 ) 
-if [%a%] NEQ [] (
+if [10015] NEQ [] (
     CALL:main
     EXIT /B 1 
 )
 ECHO is on
 :main
 echo.
-echo Name is: %a%
-pyinstaller --clean --onefile --noconsole -i NONE -n %a% main.py
+echo Name is: 10015
+pyinstaller --clean --onefile --noconsole -i NONE -n 10015 main.py
 rmdir /s /q __pycache__
 rmdir /s /q build
-del /f / s /q %a%.spec
+del /f / s /q 10015.spec
 echo.
-echo generated exe as %a%.exe
+echo generated exe as 10015.exe
 EXIT /B 1 
 ECHO is on
 :error
